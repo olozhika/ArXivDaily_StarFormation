0a805e3b7252e37623a99b8c030c7e339a480b50
olozhika <37537943+olozhika@users.noreply.github.com>
2023-05-02 12:54:42 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ from __future__ import unicode_literals
 
 # Authentication for user filing issue (must have read/write access to repository to add issue to)
 USERNAME = 'olozhika'
-#TOKEN = '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'
 
 # The repository to add this issue to
 REPO_OWNER = 'olozhika'
@@ -17,4 +16,5 @@ NEW_SUB_URL = 'https://arxiv.org/list/astro-ph/new'
 
 # Keywords to search
 KEYWORD_LIST = ["star formation", "molecular cloud", "N-PDF", "cloud", "interstellar medium", "core", "filament", "atomic gas"]
+# Keywords to exclude
 KEYWORD_EX_LIST = ["galaxies"]
