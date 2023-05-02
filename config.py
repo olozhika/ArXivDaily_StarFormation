ff6d5859bc0ab2f72db50cd605ced02188dff8f8
olozhika <37537943+olozhika@users.noreply.github.com>
2023-05-02 09:56:29 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ from __future__ import unicode_literals
 
 # Authentication for user filing issue (must have read/write access to repository to add issue to)
 USERNAME = 'olozhika'
+TOKEN = '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'
 
 # The repository to add this issue to
 REPO_OWNER = 'olozhika'
