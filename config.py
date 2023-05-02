d23b76af7035dca7ea2b296515d5bdf2a62cb0ec
olozhika <37537943+olozhika@users.noreply.github.com>
2023-05-02 09:48:21 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 # Authentication for user filing issue (must have read/write access to repository to add issue to)
-USERNAME = 'zhuhu00'
+USERNAME = 'olozhika'
 
 # The repository to add this issue to
-REPO_OWNER = 'zhuhu00'
-REPO_NAME = 'arxiv-daily-notice'
+REPO_OWNER = 'olozhika'
+REPO_NAME = 'ArXivDaily_auto'
 
 # Set new submission url of subject
-NEW_SUB_URL = 'https://arxiv.org/list/cs/new'
+NEW_SUB_URL = 'https://arxiv.org/list/astro-ph/new'
 
 # Keywords to search
-KEYWORD_LIST = ["SLAM","odometry","livox","loam","lidar","loop detection", "nerf","mapping","localization","transformer","autonomous driving"]
+KEYWORD_LIST = ["star formation", "molecular cloud", "N-PDF"]
